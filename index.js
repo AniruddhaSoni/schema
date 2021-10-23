5c19de9d67d0ee8afa817901c8207566550aa4ee
AniruddhaSoni <anirudh.soni007@gmail.com>
2021-10-24 04:20:33 +0530

    updated cors

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const apiRoute = require("./routes/Api");
 
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 app.use("/api", apiRoute);
 
